b270d5e3ba24fcdc6e14cf0be07f0298890577e0
yoshiddddd <yoshidakerapo@gmail.com>
2024-06-20 14:18:20 +0900

    add: adminuser画面

--- ari/src/app/app-routing.module.ts
+++ ari/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeScreenComponent } from './home/home-screen/home-screen.component';
+import { AdminUserComponent } from './wait-room/admin-user/admin-user.component';
 const routes: Routes = [
     {path: '', redirectTo: '/home', pathMatch: 'full'},
     {path: 'home', component: HomeScreenComponent},
+    {path: 'admin', component: AdminUserComponent}
 ];
 
 @NgModule({
--- ari/src/app/app.module.ts
+++ ari/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeScreenComponent } from './home/home-screen/home-screen.component';
+import { AdminUserComponent } from './wait-room/admin-user/admin-user.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeScreenComponent
+    HomeScreenComponent,
+    AdminUserComponent
   ],
   imports: [
     BrowserModule,
