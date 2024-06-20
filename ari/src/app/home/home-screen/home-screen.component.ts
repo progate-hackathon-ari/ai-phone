27c421a3a7eb96185eb12d86d668d3763f9afd70
murasame29 <oogiriminister@gmail.com>
2024-06-20 08:32:49 +0000

    createRoomとupdateRoomのserviceを実装

--- ari/src/app/home/home-screen/home-screen.component.ts
+++ ari/src/app/home/home-screen/home-screen.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpService } from '../../serivce/http.service';
 
 @Component({
   selector: 'app-home-screen',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrl: './home-screen.component.scss'
 })
 export class HomeScreenComponent {
+  constructor(private http: HttpService){}
 
+  creatAndJoinRoom() {
+    let room = this.http.CreateRoom();
+
+    room.subscribe(data => {
+      this.joinRoom(data.roomId);
+    })
+  }
+
+  joinRoom(roomId: string) {
+    // このタイミングでws確立させて join room を行う
+    console.log(roomId);
+  }
 }
