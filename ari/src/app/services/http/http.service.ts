e43fc073978d5e3d2f2f8782697bb0eca2c489f5
murasame29 <oogiriminister@gmail.com>
2024-06-20 12:05:49 +0000

    http serviceのdirを統一

--- ari/src/app/services/http/http.service.ts
+++ ari/src/app/services/http/http.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { environment } from '../../environments/environment';
 import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
 
 
 export type CreateRoomResponse = {
