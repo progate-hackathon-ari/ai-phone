f38d92cee8898e2873ba9f3235abb7bb5ab05b9f
yoshiddddd <yoshidakerapo@gmail.com>
2024-06-22 15:32:28 +0900

    create:resultscreen

--- ari/src/app/result-menu/result-menu.component.ts
+++ ari/src/app/result-menu/result-menu.component.ts
@@ -3,14 +3,21 @@ import {Router} from "@angular/router";
 import {GameService, dataSubscribe} from "../services/game/game.service";
 import {Observable, Subscription} from "rxjs";
 
+interface Player {
+    id: number;
+    name: string;
+    prompt: string;
+    image_uri: string;
+  }
+
 @Component({
-  selector: 'app-result-menu',
-  templateUrl: './result-menu.component.html',
-  styleUrl: './result-menu.component.scss'
+    selector: 'app-result-menu',
+    templateUrl: './result-menu.component.html',
+    styleUrl: './result-menu.component.scss'
 })
 export class ResultMenuComponent implements OnInit, OnDestroy{
-  constructor(private router: Router, private gameService: GameService, private dataSubscribe: dataSubscribe) {
-  }
+    constructor(private router: Router, private gameService: GameService, private dataSubscribe: dataSubscribe) {
+    }
   dsub: Observable<any> | undefined;
   Subs: Subscription | undefined;
 
@@ -31,5 +38,15 @@ export class ResultMenuComponent implements OnInit, OnDestroy{
     if (this.Subs) {
       this.Subs.unsubscribe();
     }
+  }
+  //仮おき
+    players: Player[] = [
+      { id: 1, name: 'Player 1', prompt: '卵を焼いているフランス人',image_uri:"assets/sample.jpeg"},
+      { id: 2, name: 'Player 2', prompt: 'world' ,image_uri:"assets/sample.jpeg"},
+      { id: 3, name: 'Player 3', prompt: 'hogehoge' ,image_uri:"assets/sample.jpeg"},
+    ];
+    selectedPlayer: Player = this.players[0];
+  selectPlayer(player: Player): void {
+    this.selectedPlayer = player;
   }
 }
