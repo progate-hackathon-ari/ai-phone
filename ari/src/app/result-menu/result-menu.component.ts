4c00c06bf9f93de63df07dc0d04ab6195b2bad42
mono0218 <mono@monokamo.com>
2024-06-22 17:05:17 +0900

    Join: ResultWebSocket

--- ari/src/app/result-menu/result-menu.component.ts
+++ ari/src/app/result-menu/result-menu.component.ts
@@ -4,11 +4,15 @@ import {GameService, dataSubscribe} from "../services/game/game.service";
 import {Observable, Subscription} from "rxjs";
 
 interface Player {
-    id: number;
-    name: string;
-    prompt: string;
-    image_uri: string;
-  }
+    id: string;
+    data: DataList[];
+}
+
+
+interface DataList{
+  prompt: string;
+  image_uri: string;
+}
 
 @Component({
     selector: 'app-result-menu',
@@ -20,6 +24,8 @@ export class ResultMenuComponent implements OnInit, OnDestroy{
     }
   dsub: Observable<any> | undefined;
   Subs: Subscription | undefined;
+  players: Player[] = []
+  selectedPlayer: Player = this.players[0];
 
   ngOnInit(): void {
     if (!this.gameService.connection) {
@@ -30,7 +36,29 @@ export class ResultMenuComponent implements OnInit, OnDestroy{
 
     this.Subs = this.dsub.subscribe(data => {
       let json = JSON.parse(data)
-      console.log(json)
+
+      if(json.state === "game_end" && json.data) {
+
+        Object.entries(json.data.result).forEach(([key, value]) => {
+          let listData: DataList[] = []
+
+          if (value != undefined) {
+            Object.entries(value).forEach(([key2, value2]) => {
+              listData.push({
+                prompt: value2.prompt,
+                image_uri: value2.image_uri
+              })
+            })
+
+            this.players.push({
+              id: key,
+              data: listData
+            })
+          }
+          console.log(this.players)
+        })
+        this.selectedPlayer = this.players[0]
+      }
     })
   }
 
@@ -39,13 +67,7 @@ export class ResultMenuComponent implements OnInit, OnDestroy{
       this.Subs.unsubscribe();
     }
   }
-  //仮おき
-    players: Player[] = [
-      { id: 1, name: 'Player 1', prompt: '卵を焼いているフランス人',image_uri:"assets/sample.jpeg"},
-      { id: 2, name: 'Player 2', prompt: 'world' ,image_uri:"assets/sample.jpeg"},
-      { id: 3, name: 'Player 3', prompt: 'hogehoge' ,image_uri:"assets/sample.jpeg"},
-    ];
-    selectedPlayer: Player = this.players[0];
+
   selectPlayer(player: Player): void {
     this.selectedPlayer = player;
   }
