2f94c1b91394e11e559a17af27f20f379a4bb696
mono0218 <mono@monokamo.com>
2024-06-23 08:35:31 +0900

    Fix: DisableButton & resultMenu

--- ari/src/app/result-menu/result-menu.component.ts
+++ ari/src/app/result-menu/result-menu.component.ts
@@ -4,85 +4,50 @@ import {GameService, dataSubscribe} from "../services/game/game.service";
 import {Observable, Subscription} from "rxjs";
 
 interface Player {
-    id: string;
-    data: DataList[];
+  id: string;
+  data: DataList[];
 }
+
 interface PerUser {
-    [key: string]: {
-        img: string;
-        answer: string;
-        username:string;
-    };
+  [key: string]: {
+    img: string;
+    answer: string;
+    username: string;
+  };
 }
+
 interface ImgScore {
-    per_user: PerUser;
-    score: number;
+  per_user: PerUser;
+  score: number;
 }
 
 interface Result {
-    [key: string]: ImgScore;
+  [key: string]: ImgScore;
 }
 
 interface DataStructure {
-    state: string;
-    data: {
-        result: Result;
-    };
+  state: string;
+  data: {
+    result: Result;
+  };
 }
-interface DataList{
+
+interface DataList {
   prompt: string;
   image_uri: string;
-user: string;
+  user: string;
   score?: number;
 }
 
-
-const data = `{
-    "state": "game_end",
-    "data": {
-        "result": {
-            "aaa": {
-                "img_score": 52,
-                "per_user": {
-                    "0": {
-                        "answer": "No answer",
-                        "img": "https://d14ubbdtfe7bkh.cloudfront.net/0190407b-703b-2b490b/8fc3464e-5d37-4ac4-bfea-5334251f81e4/0.jpg",
-                        "username": "user1"
-                    },
-                    "1": {
-                        "answer": "what",
-                        "img": "https://d14ubbdtfe7bkh.cloudfront.net/0190407b-703b-2b490b/8fc3464e-5d37-4ac4-bfea-5334251f81e4/1.jpg",
-                        "username": "user1"
-                    }
-                }
-            },
-            "bbbbbbbbb": {
-                "img_score": 64,
-                "per_user": {
-                    "0": {
-                        "answer": "d",
-                        "img": "https://d14ubbdtfe7bkh.cloudfront.net/0190407b-703b-2b490b/9a41f207-6d79-4c4d-bf4c-e3254f2029f2/0.jpg",
-                        "username": "user1"
-                    },
-                    "1": {
-                        "answer": "car",
-                        "img": "https://d14ubbdtfe7bkh.cloudfront.net/0190407b-703b-2b490b/9a41f207-6d79-4c4d-bf4c-e3254f2029f2/1.jpg",
-                        "username": "user1"
-                    }
-                }
-            }
-        }
-    }
-}`;
-
 @Component({
-    selector: 'app-result-menu',
-    templateUrl: './result-menu.component.html',
-    styleUrl: './result-menu.component.scss'
+  selector: 'app-result-menu',
+  templateUrl: './result-menu.component.html',
+  styleUrl: './result-menu.component.scss'
 })
-export class ResultMenuComponent implements OnInit, OnDestroy{
-    constructor(private router: Router, private gameService: GameService, private dataSubscribe: dataSubscribe) {
-    }
+export class ResultMenuComponent implements OnInit, OnDestroy {
+  constructor(private router: Router, private gameService: GameService, private dataSubscribe: dataSubscribe) {
+  }
+
   dsub: Observable<any> | undefined;
   Subs: Subscription | undefined;
   players: Player[] = []
@@ -98,46 +63,37 @@ export class ResultMenuComponent implements OnInit, OnDestroy{
     this.Subs = this.dsub.subscribe(data => {
         const json: DataStructure = JSON.parse(data);
         console.log(json);
-          if (json.state === "game_end" && json.data) {
-            Object.entries(json.data.result).forEach(([key, value]) => {
-                let listData: DataList[] = [];
-                let score: number | undefined;
-        
-                console.log(value);
-            if (value != undefined) {
-                if ('img_score' in value) {
-                    score = value.img_score as number;
-                    console.log("img_score 1 " + score);
-                }
-                if ('per_user' in value) {
-                    Object.entries(value.per_user as PerUser).forEach(([userKey, userValue]) => {
-                        console.log("img_score 2 " + score);
-                        console.log("key  " + userKey);
-                        console.log("value  " + userValue.answer);
-                        console.log("image  " + userValue.img);
-                        listData.push({
-                            prompt: userValue.answer,
-                            image_uri: userValue.img,
-                            user: userValue.username,
-                            score: score // img_score を同じユーザーのデータに含める
-                        });
-                    });
-                }
-                    this.players.push({
-                      id: key,
-                      data: listData
-                    })
-            }
-    
-            //   console.log(this.players)
-            })
-            
-                this.selectedPlayer = this.players[0]
-              }
-    }
-)
+        if (json.state === "game_end" && json.data) {
+          Object.entries(json.data.result).forEach(([key, value]) => {
+            let listData: DataList[] = [];
+            let score: number | undefined;
 
-    //   let json = JSON.parse(data)
+            if (value != undefined) {
+              if ('img_score' in value) {
+                score = value.img_score as number;
+              }
+              if ('per_user' in value) {
+                Object.entries(value.per_user as PerUser).forEach(([userKey, userValue]) => {
+                  listData.push({
+                    prompt: userValue.answer,
+                    image_uri: userValue.img,
+                    user: userValue.username,
+                    score: score // img_score を同じユーザーのデータに含める
+                  });
+                });
+              }
+              this.players.push({
+                id: key,
+                data: listData
+              })
+            }
+
+          })
+
+          this.selectedPlayer = this.players[0]
+        }
+      }
+    )
 
   }
 
@@ -150,6 +106,10 @@ export class ResultMenuComponent implements OnInit, OnDestroy{
   selectPlayer(player: Player): void {
     this.selectedPlayer = player;
     console.log(this.selectedPlayer);
+  }
+
+  onClickHome(){
+    this.router.navigateByUrl(`${document.location.origin}/`).then()
   }
 }
 
